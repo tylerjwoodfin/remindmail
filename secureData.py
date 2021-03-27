f8c664507f5de0c12daee81049d7d0100f5acc8a
Your Name Here (In Lights) <tyler@tylerwoodfin.com>
2021-03-27 15:20:22 -0500

    Deprecated and removed sendMail.sh

--- secureData.py
+++ secureData.py
@@ -4,7 +4,6 @@
 fileDir = "/home/pi/Git/SecureData/"
 
 def variable(item):
-    # print("Opening " + fileDir + item)
     f = open(fileDir + item, "a+")
     f.seek(0,0)
     return f.read().rstrip().split('\n')[0]
@@ -13,6 +12,11 @@ def array(item):
     f = open(fileDir + item, "a+")
     f.seek(0,0)
     return f.read().rstrip().split('\n')
+
+def file(item):
+    f = open(fileDir + item, "a+")
+    f.seek(0,0)
+    return f.read().rstrip()
 
 def write(item, content):
     f = open(fileDir + item, "w")
