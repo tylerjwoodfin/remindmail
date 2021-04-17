3d4fc4fdee82780c64c3e2c66660e8ca82c8448f
Tyler Woodfin <tyler@tylerwoodfin.com>
2021-04-17 17:44:24 -0500

    Modified tasks, secureData in preparation for google-reminders-cli integration

--- tasks.py
+++ tasks.py
@@ -1,16 +1,23 @@
 # ReadMe
 # A tool for editing Tasks.txt in Dropbox; to be integrated with Raspberry Pilot
 
-import os, sys
+import os
+import sys
+import subprocess
 
-helpText = """\nUsage: v t <command>\n\n<command>:
+sys.path.insert(0, '../google-reminders-cli')
+from secureData import *
+
+helpText = """\nUsage: rp t <command>\n\n<command>:
 	add <taskInfo>
 	add <taskInfo> <line number>
 	rm  <taskInfo>
 	rm  <line number>
 	ls
+	pull
 	config
-	help <command>
+	
+	For help with a specific command: help <command>
 	
 Parameters:
 	taskInfo: enter any task you want to complete.
@@ -19,7 +26,7 @@ Parameters:
 
 def add(s=None):
 	if(s == "help"):
-		print("Add something")
+		return "Add something"
 	print("Adding things")
 	
 def rm(s=None):
@@ -34,22 +41,31 @@ def config(s=None):
 def help():
 	func = params.get(sys.argv[2])
 	if(hasattr(func, '__name__')):
-		func("help")
+		print(func("help"))
 	else:
 		print(sys.argv[2] + " is not a command.\n")
 		print(helpText)
+
+def pull(s=None):
+	if(s == "help"):
+		return f"Pull reminders from Google Calendar and add them to Tasks.txt in secureData.noteDir (currently {noteDir})"
+		
+	print("Pulling things")
+	subprocess.call(['../google-reminders-cli/remind.py', '-l', '5'])
 	
 params = {
 	"add": add,
 	"rm": rm,
 	"ls": ls,
 	"config": config,
-	"help": help
+	"help": help,
+	"pull": pull
 }
 
 if(len(sys.argv)) == 1:
 	print(helpText)
 	quit()
 	
-func = params.get(sys.argv[1], lambda: print("Invalid"))
-func()
+if __name__ == '__main__':
+	func = params.get(sys.argv[1], lambda: print(helpText))
+	func()
