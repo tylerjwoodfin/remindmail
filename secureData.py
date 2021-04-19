ff230814f2cb61aee9123c1616f09e951cd5bd83
Tyler Woodfin <tyler@tylerwoodfin.com>
2021-04-18 20:53:52 -0500

    Fixed bug with /SecureData directory; will fix later

--- secureData.py
+++ secureData.py
@@ -5,7 +5,7 @@
 from pathlib import Path
 import os, time
 
-secureDir = str(Path(__file__).parent.absolute()) + "/SecureData"
+secureDir = "/home/pi/Git/SecureData/"
 
 # don't modify notesDir here- modify by changing {secureDir}/NotesDir
 notesDir = "/home/pi/Dropbox/Notes"
