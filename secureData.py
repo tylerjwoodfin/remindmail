a1a63bbd35b417baf1c6521873b87073d4889615
Tyler Woodfin <tyler@tylerwoodfin.com>
2021-04-18 20:25:47 -0500

    Finished tasks.py add, rm, ls, config notes

--- secureData.py
+++ secureData.py
@@ -2,55 +2,70 @@
 # Returns files from my SecureData folder (and other folders as needed)
 # Dependencies: rclone
 
+from pathlib import Path
 import os, time
 
-# don't forget the ending /
-secure = "/home/pi/Git/SecureData/"
-noteDir = "/home/pi/Dropbox/Notes/" 
+secureDir = str(Path(__file__).parent.absolute()) + "/SecureData"
+
+# don't modify notesDir here- modify by changing {secureDir}/NotesDir
+notesDir = "/home/pi/Dropbox/Notes"
+directory = __file__
 
 # prepares files for other functions
 def __initialize(item, path, action="a+"):
     if(path == "notes"):
-        path = noteDir
+        path = notesDir
     if not os.path.exists(path):
         os.makedirs(path)
+    if not os.path.exists(path + item):
+        f = open(path + item, 'w')
+        f.write('')
+        f.close()
 
-    # pull from Dropbox
-    if(path == noteDir):
+    if(path == notesDir):
+        # pull from Dropbox
         os.system("rclone copyto Dropbox:Notes/{} {}".format(item, path + item))
     return open(path + item, action)
 
 # reads the first line of a file
-def variable(item, path=secure):
+def variable(item, path=secureDir):
     f = __initialize(item, path)
     f.seek(0,0)
-    return f.read().rstrip().splitlines()[0]
+    try:
+        return f.read().rstrip().splitlines()[0]
+    except:
+        return ''
+
+# override default notes directory
+notesDir = variable("NotesDir") if len(variable("NotesDir")) > 0 else notesDir
+if(notesDir[-1] != "/"):
+    notesDir += "/"
 
 # returns the file as an array
-def array(item, path=secure):
+def array(item, path=secureDir):
     f = __initialize(item, path)
     f.seek(0,0)
     return f.read().rstrip().splitlines()
 
 # returns the file as a string without splitting
-def file(item, path=secure):
+def file(item, path=secureDir):
     f = __initialize(item, path)
     f.seek(0,0)
     return f.read().rstrip()
 
 # writes a file, replacing the contents entirely
-def write(item, content, path=secure):
+def write(item, content, path=secureDir):
     f = __initialize(item, path, "w")
     f.write(content)
     f.close()
 
     # Push to Dropbox
-    if(path == noteDir or path == "notes"):
-        path = noteDir
+    if(path == notesDir or path == "notes"):
+        path = notesDir
         os.system("rclone copyto {} Dropbox:Notes/{}".format(path + item, item))
 
 # appends a file where duplicate lines in 'content' will be removed
-def writeUnique(item, content, path=secure):
+def writeUnique(item, content, path=secureDir):
     content = file(item, path) + '\n' + content
     lines = content.splitlines()
     lines = list(dict.fromkeys(lines))
--- tasks.py
+++ tasks.py
@@ -7,24 +7,33 @@ import subprocess
 import json
 
 sys.path.insert(0, '../google-reminders-cli')
-from secureData import noteDir, writeUnique
+from secureData import notesDir, secureDir, write, writeUnique, array, directory
 from remind import tasks
 
-helpText = """\nUsage: rp t <command>\n\n<command>:
+helpText = f"""\nUsage: rp t <command>\n\n<command>:
 	add <taskInfo>
 	add <taskInfo> <line number>
 	rm  <taskInfo>
 	rm  <line number>
 	ls
 	pull
-	config
+	config notes <directory>
 	
 	For help with a specific command: help <command>
 	
 Parameters:
 	taskInfo: enter any task you want to complete.
+
+Notes Directory:
+	Tasks.txt in {notesDir}. Change the directory by modifying {secureDir}/NotesDir.
 	
 	"""
+
+def __toLower(arr):
+	for i in range(len(arr)):
+		arr[i] = arr[i].lower()
+	
+	return arr
 
 def add(s=None):
 	if(s == "help"):
@@ -32,25 +41,48 @@ def add(s=None):
 	print("Adding things")
 	
 def rm(s=None):
-	print("Removing things")
-	
+	if(s == "help"):
+		return f"Pulls latest Tasks.txt in secureData.notesDir (currently {notesDir}), then removes the selected string or index in the file.\n\ne.g. 'rp t rm 3' removes the third line.\n\nUsage: rp t rm <string matching task title, or integer of a line to remove>"
+	if(len(sys.argv) == 2):
+		print(rm("help"))
+		return
+
+	# convert list and query to lowercase to avoid false negatives
+	tasks = __toLower(array("Tasks.txt", "notes"))
+	sys.argv[2] = sys.argv[2].lower()
+
+	try:
+		del tasks[int(sys.argv[2])-1]
+		write("Tasks.txt", '\n'.join(tasks), "notes")
+		print(f"Removed {sys.argv[2]}. New Tasks:\n")
+		ls()
+	except:
+		if(sys.argv[2] in tasks):
+			tasks.remove(sys.argv[2])
+			write("Tasks.txt", '\n'.join(tasks), "notes")
+			print(f"Removed {sys.argv[2]}. New Tasks:\n")
+			ls()
+		else:
+			print(f"'{sys.argv[2]}' isn't in {notesDir}Tasks.txt.")
+
 def ls(s=None):
-	print("Listing things")
+	if(s == "help"):
+		return "Displays the latest Tasks.txt in secureData.notesDir (currently {notesDir}), as well as line numbers\n\nUsage: rp t ls"
 	
-def config(s=None):
-	print("Configuring things")
+	os.system(f"cat -n {notesDir}Tasks.txt")
+	print("\n")
 	
 def help():
-	func = params.get(sys.argv[2])
-	if(hasattr(func, '__name__')):
-		print(func("help"))
+	if(len(sys.argv) > 2):
+		func = params.get(sys.argv[2])
+		if(hasattr(func, '__name__')):
+			print(func("help"))
 	else:
-		print(sys.argv[2] + " is not a command.\n")
 		print(helpText)
 
 def pull(s=None):
 	if(s == "help"):
-		return f"Pull reminders from Google Calendar, delete them, and add them to Tasks.txt in secureData.noteDir (currently {noteDir})"
+		return f"Pull reminders from Google Calendar, delete them, and add them to Tasks.txt in secureData.notesDir (currently {notesDir})"
 		
 	print("Pulling from Google...")
 	items = tasks()
@@ -61,21 +93,33 @@ def pull(s=None):
 		print(item)
 		if(not item["done"]):
 			titlesToAdd.append(item['title'])
-			print(f"Moving {item['title']} to {noteDir}Tasks.txt")
+			print(f"Moving {item['title']} to {notesDir}Tasks.txt")
 		print(f"Deleting {item['title']}")
 		print(subprocess.check_output(['../google-reminders-cli/remind.py', '-d', item['id']]))
 
 	if(len(titlesToAdd) > 0):
 		writeUnique("Tasks.txt", '\n'.join(titlesToAdd), "notes")
+
+def config(s=None):
+	if(s == "help"):
+		return f"rp t config notes <dir>: Set your notes directory\ne.g. rp t config notes /home/pi/Dropbox/Notes"
+	if(len(sys.argv) < 4):
+		print(config("help"))
+		return
+
+	if(sys.argv[2].lower() == "notes"):
+		newDir = sys.argv[3] if sys.argv[-1] == '/' else sys.argv[3] + '/'
+		write("NotesDir", newDir)
+		print(f"Tasks will now be stored in {newDir}Tasks.txt.")
 		
 	
 params = {
 	"add": add,
 	"rm": rm,
 	"ls": ls,
-	"config": config,
 	"help": help,
-	"pull": pull
+	"pull": pull,
+	"config": config
 }
 
 if(len(sys.argv)) == 1:
