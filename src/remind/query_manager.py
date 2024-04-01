fea90edf838f90b854369e12a222eb3bed1b1fb6
Tyler Woodfin <14207553+tylerjwoodfin@users.noreply.github.com>
2024-03-31 22:52:21 -0700

    ANT-118: WIP: Parsing Updates

--- src/remind/query_manager.py
+++ src/remind/query_manager.py
@@ -2,6 +2,9 @@
 handles reminder requests
 """
 
+import re
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta, MO, TU, WE, TH, FR, SA, SU
 from remind.reminder import Reminder
 from remind.reminder_manager import ReminderManager
 from cabinet import Cabinet, Mail
@@ -23,6 +26,109 @@ class QueryManager:
         print("Not yet Implemented\n")
         print(reminder)
 
+    def interpret_reminder_date(self, input_str: str) -> Reminder:
+        """
+        Parses a reminder date from a natural language string. 
+
+        The function supports various date formats, including relative dates (e.g., "in 3 weeks"), 
+        specific calendar dates (e.g., "July 4"), and weekdays (e.g., "Tuesday"). It interprets 
+        these formats to return a Reminder object with the calculated date.
+
+        Parameters:
+        - input_str (str): The input string containing the date information.
+
+        Returns:
+        - Reminder: An object representing the reminder with the interpreted date.
+        
+        Raises:
+        - ValueError: If the input string cannot be parsed into a known date format.
+
+        TODO
+        - support for other formats like `1/2`, `3/6/2029`, etc
+        - every n days
+        - every n months
+        - offset
+        """
+
+        weekdays = {
+            "monday": MO(+1), "mon": MO(+1),
+            "tuesday": TU(+1), "tue": TU(+1),
+            "wednesday": WE(+1), "wed": WE(+1),
+            "thursday": TH(+1), "thu": TH(+1),
+            "friday": FR(+1), "fri": FR(+1),
+            "saturday": SA(+1), "sat": SA(+1),
+            "sunday": SU(+1), "sun": SU(+1),
+        }
+
+        # Precompile regular expressions
+        regex_patterns = {
+            'relative': re.compile(r'(?:in )?(\d+) (day|week|month|year)s?(?: from now)?',
+                                   re.IGNORECASE),
+            'day_of_month': re.compile(r'\b(?:the )?(\d{1,2})(st|nd|rd|th)?\b', re.IGNORECASE),
+            'every_weeks': re.compile(r'every (\d+) weeks?', re.IGNORECASE),
+            'specific_date': re.compile((r'(?i)\b(january|february|march|april|may|june|july|august'
+                            r'|september|october|november|december) (\d{1,2})(?:,? (\d{4}))?\b'))
+        }
+
+        start_date = datetime.now() + timedelta(days=1)
+        key, value, frequency, modifiers = '', '', None, 'd'
+
+        if 'every' not in input_str:
+            # relative
+            if match := regex_patterns['relative'].search(input_str):
+                number = int(match.group(1))
+                unit = match.group(2)
+                delta = {'day': relativedelta(days=number),
+                         'week': relativedelta(weeks=number),
+                         'month': relativedelta(months=number),
+                         'year': relativedelta(years=number)}[unit]
+                future_date = start_date + delta
+                if future_date <= start_date:
+                    raise ValueError((f"{future_date.strftime('%Y-%m-%d')}"
+                                      " is in the past."))
+                key, value = 'date', future_date.strftime('%Y-%m-%d')
+
+            # day of month
+            elif match := regex_patterns['day_of_month'].match(input_str):
+                key, value = 'dom', match.group(1)
+
+            # specific weekday
+            elif any(day in input_str.lower() for day in weekdays):
+                day_str = input_str.lower()
+                for day, rel_day in weekdays.items():
+                    if day in day_str:
+                        next_weekday = start_date + relativedelta(weekday=rel_day)
+                        key, value = 'date', next_weekday.strftime('%Y-%m-%d')
+                        break
+
+            # specific date
+            elif match := regex_patterns['specific_date'].search(input_str):
+                year = match.group(3) or start_date.year
+                date_str = f"{year}-{match.group(1)[:3].title()}-{match.group(2).zfill(2)}"
+                date_formatted = datetime.strptime(date_str, '%Y-%b-%d')
+                # If the interpreted date is in the past, add a year to it
+                if date_formatted <= start_date:
+                    date_formatted = date_formatted + relativedelta(years=1)
+                key, value = 'date', date_formatted.strftime('%Y-%m-%d')
+        else:
+            if match := regex_patterns['every_weeks'].search(input_str):
+                key, frequency, modifiers = 'w', int(match.group(1)), ''
+
+        if not key:
+            self.cabinet.log(f"Could not parse date: {input_str}",
+                             level="warn", is_quiet=True)
+            raise ValueError("Sorry, I didn't understand that.")
+
+        return Reminder(key=key,
+                        value=value,
+                        frequency=frequency,
+                        modifiers=modifiers,
+                        title='',
+                        notes='',
+                        offset=0,
+                        cabinet=self.cabinet,
+                        mail=self.mail)
+
     def wizard_manual_reminder(self) -> Reminder:
         """
         Guides the user through the process of creating a new manual reminder
@@ -37,26 +143,21 @@ class QueryManager:
         def _format_input(text: str) -> str:
             return input(f"{text}?\n")
 
-        title = _format_input("What's the reminder").strip()
-        reminder_date = _format_input(
-            "When do you want to be reminded").strip() or None
-        reminder_type = _format_input("reminder type (d, w, m, dow, dom, later)").strip()
-        cycle_input = _format_input("cycle or interval (number of days, optional)").strip()
-        cycle = int(cycle_input) if cycle_input.isdigit() else None
-        offset = int(_format_input("offset (default is 0)") or 0)
-        modifiers = _format_input("any modifiers (e.g., 'd' for delete, 'c' for command)").strip()
-        notes = _format_input("any notes for the reminder (optional)").strip() or None
+        title: str = _format_input("What's the reminder").strip()
+        reminder_date_success: bool = False
 
-        reminder = Reminder(reminder_type=reminder_type,
-                        reminder_date=reminder_date,
-                        cycle=cycle,
-                        offset=offset,
-                        modifiers=modifiers,
-                        title=title,
-                        notes=notes,
-                        cabinet=self.cabinet,
-                        mail=self.mail)
+        while not reminder_date_success:
+            reminder_date: str = _format_input(
+                "When do you want to be reminded").strip() or None
 
-        self.write_reminder_to_file(reminder)
+            try:
+                reminder: Reminder = self.interpret_reminder_date(reminder_date)
+                reminder.title = title
+                reminder_date_success = True
+            except ValueError as e:
+                print(e)
+
+        print(reminder)
+        # self.write_reminder_to_file(reminder)
 
         return reminder
